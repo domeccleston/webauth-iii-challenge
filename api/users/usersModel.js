dc3f001de2fce640274ca9c4e24caa6438e015ca
Dom <domeccleston@gmail.com>
2019-11-13 15:41:28 +0000

    restricted users endpoint with jwt

--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -1,14 +1,19 @@
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
 
 function getUsers() {
-    return db('users');
+  return db("users");
 }
 
 function createUser(userDetails) {
-    return db('users').insert(userDetails);
+  return db("users").insert(userDetails);
+}
+
+function findBy(parameter) {
+  return db("users").where(parameter);
 }
 
 module.exports = {
-    getUsers,
-    createUser,
-}
+  getUsers,
+  createUser,
+  findBy
+};
